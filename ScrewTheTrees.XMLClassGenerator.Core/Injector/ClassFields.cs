23f2af574e205d6c203db78c20523460eae9886e
Fredrik Grimmenhag <fredrik@grimmenhag.se>
2017-10-09 10:01:17 +0200

    Basic List Injection logic

--- ScrewTheTrees.XMLClassGenerator.Core/Injector/ClassFields.cs
+++ ScrewTheTrees.XMLClassGenerator.Core/Injector/ClassFields.cs
@@ -1,12 +1,56 @@
 ﻿
 
+using System.Collections.Generic;
+
 namespace ScrewTheTrees.XmlClassGenerator.Core
 {
     public class ClassFields
     {
         public string FieldName;
+        public string FieldType;
+
         public string NewName;
         public string NewType;
+
         public string Comment;
+
+        public ClassFields(string fieldName, string fieldType, string newName, string newType)
+        {
+            FieldName = fieldName;
+            FieldType = fieldType;
+            NewName = newName;
+            NewType = newType;
+        }
+
+        /// <summary>
+        /// Injects this ClassField into a string List
+        /// </summary>
+        /// <param name="list">List to Inject into.</param>
+        /// <returns>Returns whenever it was successfully injected or not.</returns>
+        public bool InjectIntoList(List<string> list)
+        {
+            string comment;
+            if (Comment != null)
+                comment = "//" + Comment;
+            else comment = "";
+
+
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                string tempString = list[i];
+
+                tempString = tempString.Trim(' ');
+
+                if (tempString.StartsWith(string.Format("{0} {1};" , FieldType, FieldName)))
+                {
+                    list[i] = string.Format("{0} {1}; {2}", FieldType, FieldName, comment);
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
--- ScrewTheTrees.XMLClassGenerator.Core/Injector/ClassIncludes.cs
+++ ScrewTheTrees.XMLClassGenerator.Core/Injector/ClassIncludes.cs
@@ -1,8 +1,33 @@
 ﻿
+using System.Collections.Generic;
+
 namespace ScrewTheTrees.XmlClassGenerator.Core
 {
     public class ClassIncludes
     {
+        public string IncludeString;
+        public string Comment;
 
+        public ClassIncludes(string includeString)
+        {
+            IncludeString = includeString;
+        }
+
+        /// <summary>
+        /// Injects this Include into a string List
+        /// </summary>
+        /// <param name="list">List to Inject into.</param>
+        /// <returns>Returns whenever it was successfully injected or not.</returns>
+        public bool InjectIntoList(List<string> list)
+        {
+            string comment;
+            if (Comment != null)
+                comment = "//" + Comment;
+            else comment = "";
+
+            list.Add( string.Format("{0} {1}", IncludeString, comment));
+
+            return true;
+        }
     }
 }
